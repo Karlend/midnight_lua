ff7f2b7edbe0684d087050f793ef24666716bab1
Johnny <wreckerplayer@gmail.com>
2021-12-11 14:53:56 +0200

    500k loop -> 750k loop

--- gta/money_loop.lua
+++ gta/money_loop.lua
@@ -3,12 +3,10 @@ function OnFeatureTick()
 	local now = system.ticks()
 	if next > now then return end
 	next = now + 25000
-	local shirt = script_global:new(2520153)
-	shirt:set_float(0)
 
-	local amount = script_global:new(262145):at(22765)
-	amount:set_int64(500000)
+	local amount = script_global:new(262145):at(22766)
+	amount:set_int64(750000)
 
 	local receive = script_global:new(1700984)
-	receive:set_long(1)
+	receive:set_long(2)
 end
