10266c5452e880ec6bb7a6a87afded9794b737e7
Johnny <wreckerplayer@gmail.com>
2021-12-15 11:00:23 +0200

    Updated loop method 750k -> 500k

--- gta/money_loop.lua
+++ gta/money_loop.lua
@@ -3,10 +3,18 @@ function OnFeatureTick()
 	local now = system.ticks()
 	if next > now then return end
 	next = now + 25000
-
-	local amount = script_global:new(262145):at(22766)
-	amount:set_int64(750000)
+	local lp = player.index()
+	if not player.is_valid(lp) or not player.is_connected(lp) then
+		return
+	end
 
 	local receive = script_global:new(1700984)
-	receive:set_long(2)
+	receive:set_long(1)
+end
+
+function OnWarningScreen(thread)
+	if thread == "shop_controller" then
+		print("Got transaction error")
+		next = system.ticks() + 25000
+	end
 end
